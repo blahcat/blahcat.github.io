dcc875aeaee057dbf98a8a8dda22c1a2e916f234
hugsy <hugsy@blah.cat>
2023-04-04 11:05:12 -0700

    updated pelican config for atom feed link from the sidebar

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,29 +4,29 @@ SITEURL = ''
 SITESUBTITLE = 'Tales of a binary encoded life...'
 PATH = 'content'
 
-TIMEZONE              = 'America/Vancouver'
-DEFAULT_LANG          = 'en'
-DEFAULT_DATE          =  'fs'
-DEFAULT_DATE_FORMAT   = '%d %b %Y'
+TIMEZONE = 'America/Vancouver'
+DEFAULT_LANG = 'en'
+DEFAULT_DATE = 'fs'
+DEFAULT_DATE_FORMAT = '%d %b %Y'
 
 # Feed generation is usually not desired when developing
-FEED_MAX_ITEMS           = 15
-FEED_ALL_ATOM            = '/feeds/all.atom.xml'
-CATEGORY_FEED_ATOM       = '/feeds/{slug}.atom.xml'
-TRANSLATION_FEED_ATOM    = None
-AUTHOR_FEED_ATOM         = None
-AUTHOR_FEED_RSS          = None
+FEED_MAX_ITEMS = 15
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = 'feeds/author.atom.xml'
+AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-  ('TheGoonies CTF', 'https://thegooniesctf.github.io/'),
-  ('Bernardo', 'https://w00tsec.blogspot.com/'),
-  ('Danilo', 'https://bugnotfound.com'),
-  ('grazfather', 'http://grazfather.github.io/'),
-  ('Diary of a Reverse Engineer', 'https://doar-e.github.io/'),
-  ('Connor McGarr', 'https://connormcgarr.github.io'),
-  ('tiraniddo.dev', 'https://tiraniddo.dev'),
-  ('zodiacon\'s blog', 'https://scorpiosoftware.net/')
+    ('TheGoonies CTF', 'https://thegooniesctf.github.io/'),
+    ('Bernardo', 'https://w00tsec.blogspot.com/'),
+    ('Danilo', 'https://bugnotfound.com'),
+    ('grazfather', 'http://grazfather.github.io/'),
+    ('Diary of a Reverse Engineer', 'https://doar-e.github.io/'),
+    ('Connor McGarr', 'https://connormcgarr.github.io'),
+    ('tiraniddo.dev', 'https://tiraniddo.dev'),
+    ('zodiacon\'s blog', 'https://scorpiosoftware.net/')
 )
 
 # Social widget
@@ -51,15 +51,15 @@ MENUITEMS = (
     ('Qemu VMs', '/pages/qemu-vm-repo'),
     ('Exploitation Tutorials', '/pages/exploitation-series'),
     ('Misc Notes', '/pages/notes'),
-    ('RSS Feed', FEED_ALL_ATOM),
+    ('RSS Feed', '/'+FEED_ALL_ATOM),
 )
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 PLUGINS = [
-  'sitemap',
-  'neighbors',
+    'sitemap',
+    'neighbors',
 ]
 
 STATIC_PATHS = [
@@ -89,26 +89,26 @@ SITEMAP = {
 }
 
 # Post and Pages path
-ARTICLE_URL              = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
-ARTICLE_SAVE_AS          = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
-PAGE_URL                 = 'pages/{slug}.html'
-PAGE_SAVE_AS             = 'pages/{slug}.html'
-YEAR_ARCHIVE_SAVE_AS     = 'archives/{date:%Y}/index.html'
-MONTH_ARCHIVE_SAVE_AS    = 'archives/{date:%Y}/{date:%m}/index.html'
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+YEAR_ARCHIVE_SAVE_AS = 'archives/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'archives/{date:%Y}/{date:%m}/index.html'
 
 # Tags and Category path
-CATEGORY_URL          = 'category/{slug}'
-CATEGORY_SAVE_AS      = 'category/{slug}/index.html'
-CATEGORIES_SAVE_AS    = 'categories/index.html'
-TAG_URL               = 'tag/{slug}'
-TAG_SAVE_AS           = 'tag/{slug}/index.html'
-TAGS_SAVE_AS          = 'tags/index.html'
-DRAFT_URL             = 'drafts/{slug}.html'
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags/index.html'
+DRAFT_URL = 'drafts/{slug}.html'
 
 # Author
-AUTHOR_URL           = 'author/{slug}'
-AUTHOR_SAVE_AS       = 'author/{slug}/index.html'
-AUTHORS_SAVE_AS      = 'authors.html'
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
 
 CSS_OVERRIDE = [
     'https://fonts.googleapis.com/css?family=Roboto',
@@ -124,15 +124,15 @@ JS_OVERRIDE = [
 THEME = 'attila'
 HEADER_COVER = 'assets/images/blog-cover.png'
 AUTHORS_BIO = {
-  "hugsy": {
-    "name": "hugsy",
-    "cover": "https://i.imgur.com/XrHeJlW.png",
-    "image": "assets/images/authors/hugsy.png",
-    "website": "/author/hugsy",
-    "twitter": "_hugsy_",
-    "github": "hugsy",
-    "bio": "BWAAAAHHHH"
-  }
+    "hugsy": {
+        "name": "hugsy",
+        "cover": "https://i.imgur.com/XrHeJlW.png",
+        "image": "assets/images/authors/hugsy.png",
+        "website": "/author/hugsy",
+        "twitter": "_hugsy_",
+        "github": "hugsy",
+        "bio": "BWAAAAHHHH"
+    }
 }
 
 COLOR_SCHEME_CSS = 'github.css'
